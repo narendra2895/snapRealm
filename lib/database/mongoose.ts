4896da8102fe0470396bb1a030fd21b6f3beb008
narendra2895 <106621535+narendra2895@users.noreply.github.com>
2024-05-15 03:25:11 +0100

    implementing webhooks 4

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,32 +1,34 @@
-import mongoose, {Mongoose} from "mongoose";
+import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URL =process.env.MONGODB_URL;
-
-
+const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
-let cached : MongooseConnection = (global as any).mongoose
+let cached: MongooseConnection = (global as any).mongoose
 
-if(!cached){
-    cached=(global as any).mongoose= {
-        conn:null, promise:null
-    }
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
-export const connectToDatabase =async()=>{
-    if(cached.conn) return cached.conn;
-
+export const connectToDatabase = async () => {
+    if (cached.conn) return cached.conn;
+  
     if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
-
-    cached.promise = cached.promise ||  mongoose.connect(MONGODB_URL,
-         {dbName:  'snaprealm', bufferCommands:false})
-
-
-         cached.conn = await cached.promise;
-
-         return cached.conn
-}
+  
+    try {
+      cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
+        dbName: 'snaprealm', bufferCommands: false
+      });
+      cached.conn = await cached.promise;
+      return cached.conn;
+    } catch (error) {
+      console.error("Failed to connect to MongoDB:", error);
+      throw error;
+    }
+  }
+  
